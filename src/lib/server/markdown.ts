16fb07044ef2670464357005f607547d0fd9f158
Matteo Contrini <matteo@contrini.it>
2025-09-26 11:25:04 +0200

    Add plain text description in feed

--- src/lib/server/markdown.ts
+++ src/lib/server/markdown.ts
@@ -2,14 +2,44 @@ import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
+import preview, { textFormatter } from 'remark-preview';
 
-// prettier-ignore
 const processor = unified()
 	.use(remarkParse)
+	.use(
+		preview(
+			textFormatter({
+				length: 250,
+				maxBlocks: 2
+			})
+		)
+	)
 	.use(remarkRehype)
 	.use(rehypeStringify);
 
-export async function renderMarkdown(markdown: string): Promise<string> {
+async function processMarkdown(markdown: string) {
 	const file = await processor.process(markdown);
-	return file.toString();
+	const data = file.data as PreviewData;
+	return {
+		html: file.toString(),
+		preview: data.fm?.preview ?? ''
+	};
 }
+
+export async function renderMarkdown(markdown: string): Promise<string> {
+	const { html } = await processMarkdown(markdown);
+	return html;
+}
+
+export async function renderMarkdownWithPreview(markdown: string): Promise<{
+	html: string;
+	preview: string;
+}> {
+	return processMarkdown(markdown);
+}
+
+type PreviewData = {
+	fm?: {
+		preview?: string;
+	};
+};
