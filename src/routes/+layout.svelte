61089028b232e5218cfa62e19abc4d6e3cdd4ca5
Matteo Contrini <matteo@contrini.it>
2025-09-26 14:47:50 +0200

    Update top links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,21 +22,6 @@
 
 <div class="bg-[#0067B9] h-[18px]">
 	<div class="container max-w-6xl h-full flex items-center justify-end gap-4">
-		<a
-			class="text-white/80 text-xs uppercase hover:text-white"
-			href="https://matteosonoio.it"
-		>
-			blog
-			<span class="font-[system-ui]">↗︎</span>
-		</a>
-
-		<a
-			class="text-white/80 text-xs uppercase hover:text-white"
-			href={resolve('/about')}
-		>
-			about
-		</a>
-
 		{#if isLoggedIn}
 			<a
 				class="text-white/80 text-xs uppercase hover:text-white"
@@ -45,25 +30,33 @@
 				unpublished
 			</a>
 
-				<a
-					class="text-white/80 text-xs uppercase hover:text-white"
-					href={resolve('/unpublished/new')}
-				>
-					new
-				</a>
+			<a
+				class="text-white/80 text-xs uppercase hover:text-white"
+				href={resolve('/unpublished/new')}
+			>
+				new
+			</a>
 
-				<a
-					class="text-white/80 text-xs uppercase hover:text-white"
-					href={resolve('/logout')}
-				>
-					logout
-				</a>
+			<a
+				class="text-white/80 text-xs uppercase hover:text-white"
+				href={resolve('/logout')}
+			>
+				logout
+			</a>
 		{:else}
 			<a
 				class="text-white/80 text-xs uppercase hover:text-white"
-				href={resolve('/login')}
+				href="https://matteosonoio.it"
 			>
-				login
+				blog
+				<span class="font-[system-ui]">↗︎</span>
+			</a>
+
+			<a
+				class="text-white/80 text-xs uppercase hover:text-white"
+				href={resolve('/about')}
+			>
+				about
 			</a>
 		{/if}
 	</div>
